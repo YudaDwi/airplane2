8d3f25ac17be6b39b94876cc91b52a0a38f418af
YudaDwi <yudadwi0@gmail.com>
2021-09-17 09:53:21 +0700

    2 set theme

---
+++ airplane2/lib/shared.dart/theme.dart
@@ -0,0 +1,24 @@
+part of 'shared.dart';
+
+//mungkin agar lebih enak memanggilnya jadi tinggal k warna
+Color kPrimaryColor = Color(0xff5C40CC);
+Color kBlackColor = Color(0xff1F1449);
+Color kWhiteColor = Color(0xffFFFFFF);
+Color kGreyColor = Color(0xff9698A9);
+Color kGreenColor = Color(0xff0EC3AE);
+Color kRedColor = Color(0xffEB70A5);
+
+Color kBackgroundColor = Color(0xffFAFAFA);
+//warna pd stroke textfield
+Color kInactiveColor = Color(0xff5C40CC);
+
+//mungkin akan jadi fontweight standar
+FontWeight thin = FontWeight.w100;
+FontWeight extraLight = FontWeight.w200;
+FontWeight light = FontWeight.w300;
+FontWeight reguler = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
